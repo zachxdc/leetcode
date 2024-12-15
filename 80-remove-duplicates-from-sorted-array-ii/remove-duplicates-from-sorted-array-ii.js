86732824dc6378913fdf4baa903166de11b8feaf
Zach Chen <zchen@seek.com.au>
2024-12-15 23:04:40 +1100

    Time: 66 ms (36.92%) | Memory: 52.1 MB (49.55%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums.length <=2) return nums.length;
+
+    let i = 2
+
+    for (let j = 2; j<nums.length; j++){
+        if(nums[j] !== nums[i - 2]){
+            nums[i] = nums[j]
+            i++
+        } 
+    }
+return i
+    
+    
+    
+};
