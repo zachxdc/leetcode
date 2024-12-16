729667d8c67891b0ae9fe83bd27c08b9cd3611ac
Zach Chen <zchen@seek.com.au>
2024-12-17 00:06:34 +1100

    Time: 4 ms (62.55%) | Memory: 52.9 MB (33.84%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,12 +3,8 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const count = {};
-    const threshold = Math.floor(nums.length / 2);
-    for(let num of nums) {
-        count[num] = (count[num] || 0) + 1;
-        if (count[num] > threshold) {
-            return num;
-        }
-    } 
+    nums.sort((a,b) => a-b);
+    return nums[Math.floor(nums.length / 2)]; 
 };
+
+
