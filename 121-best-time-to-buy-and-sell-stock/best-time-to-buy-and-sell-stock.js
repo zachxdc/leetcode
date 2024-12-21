d99b56c0fb5b8bc86aa24b7a32530661bac5d8d2
Zach Chen <zchen@seek.com.au>
2024-12-22 01:15:18 +1100

    Time: 6 ms (17.63%) | Memory: 59.7 MB (18.10%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = Infinity; // 初始化最低价格为正无穷
-    let maxProfit = 0;       // 初始化最大利润为0
+    let minPrice = Infinity;
+    let maxProfit = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            // 更新最低价格
-            minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfit) {
-            // 计算利润并更新最大利润
-            maxProfit = prices[i] - minPrice;
+    for (let price of prices){
+        if(price<minPrice) {
+            minPrice = price;
+        } else {
+            maxProfit = Math.max(maxProfit, price-minPrice)
         }
     }
     return maxProfit;
+
 };
