d83eb864216d1fa9e90c60c59690dc366ad63a05
Zach Chen <zchen@seek.com.au>
2025-02-06 22:47:27 +1100

    Time: 82 ms (42.37%) | Memory: 56.4 MB (49.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -3,28 +3,47 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    if(!grid || grid.length === 0) return 0;
+    if(!grid || grid.length ===0) return 0;
 
-    let rows =  grid.length, cols = grid[0].length;
+    let rows = grid.length, cols = grid[0].length;
     let count = 0;
 
-    const dfs = (r,c)=>{
-        if (r<0 || c<0 || r>= rows || c>=cols || grid[r][c] === '0') return;
-        grid[r][c]='0'
+    const direction = [
+        [1, 0], [-1, 0], [0, 1], [0, -1]
+    ]
 
-        dfs(r+1,c);
-        dfs(r-1,c);
-        dfs(r,c+1);
-        dfs(r,c-1);
-    };
+    const bfs = (r,c)=> {
+        let queue = [[r,c]];
+        grid[r][c] = '0';
 
-    for(let r=0; r<rows; r++) {
-        for(let c=0; c<cols; c++) {
-            if (grid[r][c] === '1') {
-                count++;
-                dfs(r, c);
+        while (queue.length > 0) {
+            let[curR, curC] = queue.shift();
+
+        for(let[dr, dc] of direction) {
+            let newR = curR + dr, newC = curC + dc;
+
+            if (newR >= 0 && newR < rows && newC >=0 &&  newC < cols && grid[newR][newC] === '1') {
+                grid[newR][newC]='0'
+                queue.push([newR, newC]);
+
             }
         }
     }
-    return count;
+}
+        for(let r=0; r<rows; r++) {
+            for (let c=0; c<cols; c++) {
+                if(grid[r][c] ==='1'){
+                    count++
+                    bfs(r,c);
+                }
+            }
+        }
+
+        return count;
+
 };
+
+
+
+
+    
