a6fa1904e36ebf0bb1e16d531c01f6bf5435bfb9
Zach Chen <zchen@seek.com.au>
2025-02-11 00:17:10 +1100

    Time: 774 ms (54.33%) | Memory: 190.1 MB (27.84%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+
+    const graph = new Map();
+    for(const [a,b] of edges){
+        if(!graph.has(a)) graph.set(a,[]);
+        if(!graph.has(b)) graph.set(b,[]);
+        graph.get(a).push(b);
+        graph.get(b).push(a);
+    }
+
+    const visited = new Set();
+
+    const dfs = (node)=>{
+        if (node ===destination) return true;
+        visited.add(node);
+
+        for(const neighbor of graph.get(node) || []){
+            if(!visited.has(neighbor)){
+                if(dfs(neighbor)) return true;
+            }
+        }
+        return false;
+    }
+    return dfs(source);
+    
+};
